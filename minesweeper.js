fa55a990974a3bffe0db88b19a9ed4f5c775fd11
MeganPhillips <88347554+MeganPhillips@users.noreply.github.com>
2024-04-27 02:40:13 -0700

    fixed board sizing and added clickTile and the addMines function

--- minesweeper.js
+++ minesweeper.js
@@ -3,13 +3,27 @@ let rows = 9;
 let columns = 9;
 
 let mines = 10;
-// let location = [];
+let mineLocation = [];
 let flag = false;
 
 let gameOver = false;
 
 window.onload = function(){
     startGame();
+}
+
+function addMines(){
+    let minesRemain = mines;
+    while(minesRemain > 0){
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        let id = r.toString() + "-" + c.toString();
+
+        if(!mineLocation.includes(id)){
+            mineLocation.push(id);
+            minesRemain --;
+        }
+    }
 }
 
 function startGame(){
@@ -24,13 +38,33 @@ function startGame(){
             //<div id="0-0"></div>
             let tile = document.createElement("div");
             tile.id = i.toString() + "-" + j.toString();
-            // tile.addEventListener("click", clickTile);
+            tile.addEventListener("click", clickTile);
             document.getElementById("board").append(tile);
             row.push(tile);
         }
         board.push(row);
     }
 
-    console.log(board);
+    // console.log(board);
 }
-      
+
+
+function clickTile(){
+    let tile = this;
+
+    if(flagEnabled){
+        if(tile.innerText == ""){
+            tile.innerText = "🚩";
+        }
+        else if(tile.innerText == "🚩"){
+            tile.innerText = "";
+        }
+
+        if(mineLocation.includes(tile.id))
+
+        gameOver = true;
+        showMines();
+        return;
+    }
+}     
+
