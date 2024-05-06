bf7ac81fd52a5ae16bd1a5eb52f6aa3b0d76175b
Claire Hallock <claireehallock@gmail.com>
2024-05-06 10:54:48 -0700

    Removed socket in 1p

--- minesweeper/static/minesweeper.js
+++ minesweeper/static/minesweeper.js
@@ -10,13 +10,6 @@ let flag = false;
 
 let gameOver = false;
 
-
-
-var socket = io();
-    socket.on('connect', function() {
-        socket.emit('my event', {"data": "'I'm connected!'"});
-    });
-
 window.onload = function(){
     startGame();
 }
